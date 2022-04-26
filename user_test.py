bb04c63bca754cba153266feb04f0048bb0f0c40
Wamaitha <wamaithaweru19@gmail.com>
2022-04-26 09:20:38 +0300

    add second test

--- user_test.py
+++ user_test.py
@@ -16,5 +16,22 @@ class TestUser(unittest.TestCase):
                 self.assertEqual(self.new_user.pass_word,"b345")
                 self.assertEqual(self.new_user.app_name,"Todolist")
 
+ # second testcase
+    # a test to save User objects
+        def test_save(self):
+          """
+        to test if credentials are saved in the password locker
+        """
+          self.new_user.save_user() #saving the new credentials
+          self.assertEqual(len(User.user_list),1)
+
+           # second testcase
+    # a test to save User objects
+        def tearDown(self):
+              '''
+            tearDown method that does clean up after each test case has run.
+            '''
+        User.user_list = []
+
 if __name__ == '__main__':
     unittest.main()
