e4d9ce95b4bbd0dae5619426a3005a7bac464e7b
Wamaitha <wamaithaweru19@gmail.com>
2022-04-26 11:13:25 +0300

    add third test case

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 class User():
-    def __init__(self,user_name,pass_word,app_name):
+    def __init__(self,user_name,pass_word,appname):
        self.user_name = user_name
        self.pass_word = pass_word
-       self.app_name = app_name
+       self.appname = appname
 
     user_list = []
     # for the 2 test case
@@ -10,5 +10,27 @@ class User():
 
              
              User.user_list.append(self)
-            # for  third test case
-    
+        
+        # for  fourth test case
+# to find user detail@classmethod
+    @classmethod
+    def find_by_name(cls,string):
+
+
+        for user in cls.user_list:
+         if user.appname == string:
+                return user
+
+        
+
+
+
+
+    # def delete_user(self):
+
+    #      '''
+    #     delete_contact method deletes a saved contact from the contact_list
+    #     '''
+
+    #      User.user_list.remove(self)
+
--- user_test.py
+++ user_test.py
@@ -1,3 +1,4 @@
+from typing import Any
 import unittest
 from user import User
 
@@ -14,7 +15,7 @@ class TestUser(unittest.TestCase):
         def test_start(self):
                 self.assertEqual(self.new_user.user_name,"bettyweru")
                 self.assertEqual(self.new_user.pass_word,"b345")
-                self.assertEqual(self.new_user.app_name,"Todolist")
+                self.assertEqual(self.new_user.appname,"Todolist")
 
  # second testcase
         def test_save(self):
@@ -30,7 +31,7 @@ class TestUser(unittest.TestCase):
             tearDown method that does clean up after each test case has run.
             '''
             User.user_list = []
-            
+
         def test_save_multiple_user(self):
             """
             test_save_multiple_user to save multiple user details
@@ -40,25 +41,38 @@ class TestUser(unittest.TestCase):
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
 
+# delete user details test
+#         def test_delete_user(self):
+#               '''
+#                test_delete_user to test if we can remove a contact from our contact list
+#               '''
+#               self.new_user.save_user()
+#               test_user = User("Grace","Graceeru","Gmail") # new contact
+#               test_user.save.user()
+
+#               self.new_user.delete_user()# Deleting a contact object
+#               self.assertEqual(len(User.user_list),1)
+     
+    #  to find app name
+
+        def test_find_user_by_appname(self):
+          '''
+          test to check if we can find a user by appname and display information
+           '''
+
+          self.new_user.save_user()
+          test_user = User("Grace","Graceeru","Gmail") # new contact
+          test_user.save_user()
+
+          found_user = User.find_by_name("Gmail")
+                 
+          self.assertEqual(found_user.appname,test_user.appname)
+        
         
 
-   
-      
-
-      
 
 
-# delete user details test
-# def test_delete_user(self):
-#             '''
-#             test_delete_user to test if we can remove a contact from our contact list
-#             '''
-#             self.new_user.save_user()
-#             test_user = User("Test","user","0712345678","test@user.com") # new contact
-#             test_user.save.user()
 
-#             self.new_user.delete_user()# Deleting a contact object
-#             self.assertEqual(len(User.user_list),1)
-
+        
 if __name__ == '__main__':
     unittest.main()
